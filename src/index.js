9667889d8c7eb46c82ddc9d53f87f47ae5fdaa53
tania <masiukevich@gmail.com>
2018-09-14 00:26:59 +0300

    Updated function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+    var arrayOfStrings = equation.split(' ');
+    var numberArray = [];
+    var a = arrayOfStrings[0];
+    var b = arrayOfStrings[3] + arrayOfStrings[4];
+    var c = arrayOfStrings[7] + arrayOfStrings[8];
+    numberArray.push(a, b, c);
+    var d = numberArray[1] * numberArray[1] - 4 * numberArray[0] * numberArray[2];
+    var x1 = (-b + Math.sqrt(d)) / (2 * a);
+    var x2 = (-b - Math.sqrt(d)) / (2 * a);
+    var ordArray = [];
+    ordArray.push(Math.round(x1), Math.round(x2));
+    function sortNumber(a,b) {
+        return a - b;
+    }
+    return ordArray.sort(sortNumber);
 }
+
+
+
+
